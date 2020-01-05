80fc98fdb0079bce1fb756713f9264b021c8ed01
Sean Brage <seanmbrage@me.com>
2020-01-05 09:49:17 -0700

    initial commit w/ course templates and dummy routes

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/question')
+def question():
+    return render_template('question.html')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
+
+@app.route('/ask')
+def ask():
+    return render_template('ask.html')
+
+@app.route('/unanswered')
+def unanswered():
+    return render_template('unanswered.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+
+if __name__ == '__main__':
+    app.run()
